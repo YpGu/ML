dd75d83045bc8ed2733ba2f2e9d572bb6fe8f649
YpGu <gyp7364@gmail.com>
2015-03-02 17:32:31 -0500

    run.sh changed

--- hw2/src/PolynomialRegression-yacht/run.sh
+++ hw2/src/PolynomialRegression-yacht/run.sh
@@ -4,5 +4,8 @@ echo 'Compiling...'
 javac *.java
 echo 'Compile done'
 
-java PolynomialRegression ../../data/yachtData.csv 7
+for i in `seq 1 7`; do
+	echo $i
+	java PolynomialRegression ../../data/yachtData.csv $i
+done
 
