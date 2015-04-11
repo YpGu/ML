0960d0bf8baf28ffb30c1a51517d2d666f2d47a6
YpGu <gyp7364@gmail.com>
2015-04-11 15:48:13 -0400

    perceptron

---
+++ hw5/src/Perceptron/FileParser.java
@@ -0,0 +1,110 @@
+/**
+	FileReader.java: read input files
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class FileParser
+{
+	public static int readNumOfFeatures(String fileDir, String delimiter) {
+		int numAttrs = -1;
+		try (BufferedReader br = new BufferedReader(new FileReader(fileDir))) {
+			String currentLine = br.readLine();
+			String[] tokens = currentLine.split(delimiter);
+			numAttrs = tokens.length-1;
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return numAttrs;
+	}
+
+	public static int readNumOfInstances(String fileDir, String delimiter) {
+		int numInstances = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(fileDir))) {
+			String currentLine;
+			while ((currentLine = br.readLine()) != null) {
+				if (currentLine.equals(""))
+					continue;
+				numInstances += 1;
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return numInstances;
+	}
+
+	public static void readData(String fileDir, String delimiter, double[][] inputs, double[] outputs) {
+		try (BufferedReader br = new BufferedReader(new FileReader(fileDir))) {
+			String currentLine;
+			int lineID = 0;
+			while ((currentLine = br.readLine()) != null) {
+				if (currentLine.equals(""))
+					continue;
+
+				// parse line here
+				String[] tokens = currentLine.split(delimiter);
+				int i = 0;
+				for (; i < Perceptron.K; i++) {
+					double curAttr = Double.parseDouble(tokens[i].trim());
+					inputs[lineID][i] = curAttr;
+				}
+				inputs[lineID][i] = 1;
+				outputs[lineID] = Double.parseDouble(tokens[i].trim());
+				lineID++;
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return;
+	}
+
+	// Output
+	public static void output(String outputDir, double[] arr) {
+		int N = arr.length;
+		try (PrintWriter writer = new PrintWriter(outputDir, "UTF-8")) {
+			for (int x = 0; x < N; x++)
+				writer.printf("%d\t%f\n", x, arr[x]);
+		}
+		catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return;
+	}
+
+	public static void output(String outputDir, ArrayList<Double> arr) {
+		int N = arr.size();
+		try (PrintWriter writer = new PrintWriter(outputDir, "UTF-8")) {
+			for (int x = 0; x < N; x++)
+				writer.printf("%d\t%f\n", x, arr.get(x));
+		}
+		catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return;
+	}
+
+
+/*	public static void crossValidation(ArrayList<DataType> data, ArrayList<ArrayList<DataType>> cv, int num)
+	{
+		for (int i = 0; i < num; i++)
+		{
+			cv.add(new ArrayList<DataType>());
+		}
+
+		for (int i = 0; i < data.size(); i++)
+		{
+			int ind = i%num;
+			cv.get(ind).add(data.get(i));
+		}
+	}
+*/		
+}
---
+++ hw5/src/Perceptron/Perceptron.java
@@ -0,0 +1,91 @@
+/**
+	Perceptron.java: implementation of perceptron algorithm
+**/
+
+import java.util.*;
+
+public class Perceptron
+{
+	public static double[][] data;
+	public static double[] label;
+	public static double[] w;
+	public static final int NUM_FOLD = 10;
+	public static final double eta = 0.001;
+	public static int N, K;					// #data, #features 
+
+	public static void
+	init(String[] args) {
+		String fileDir = args[0];
+		N = FileParser.readNumOfInstances(fileDir, ",");
+		K = FileParser.readNumOfFeatures(fileDir, ",");
+
+		data = new double[N][K+1];
+		label = new double[N];
+		w = new double[K+1];
+		FileParser.readData(fileDir, ",", data, label);
+	}
+
+	public static double
+	innerProduct(double[] arr1, double[] arr2) {
+		int K = arr1.length;
+		if (K != arr2.length) {
+			System.out.println("Inner Product: dimension does not agree!");
+			return -1;
+		}
+
+		double res = 0;
+		for (int k = 0; k < K; k++) 
+			res += arr1[k] * arr2[k];
+		return res;
+	}
+
+	public static void
+	train(int fold) {
+		boolean flag = true;
+		while (true) {
+			for (int i = 0; i < N; i++) {
+				if (i%NUM_FOLD == fold) continue;
+				double yi = innerProduct(w, data[i]);
+				if (yi * label[i] <= 0) {
+					for (int k = 0; k < K+1; k++) 
+						w[k] += eta * label[i] * data[i][k];
+					flag = false;
+				}
+			}
+			if (flag) break;
+		}
+
+		return;
+	}
+
+	public static void
+	test(int fold) {
+		int cor = 0, tot = 0;
+		for (int i = 0; i < N; i++) {
+			if (i%NUM_FOLD != fold) continue;
+			double yi = innerProduct(w, data[i]);
+			if (yi * label[i] > 0) cor += 1;
+			tot += 1;
+		}
+		System.out.println(cor + " out of " + tot + " correct! Accuracy = " + (double)cor/tot);
+
+		return;
+	}
+
+	public static void
+	main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: java Perceptron ../../data/perceptronData.csv");
+			System.exit(0);
+		}
+
+		init(args);
+		for (int fold = 0; fold < NUM_FOLD; fold++) {
+			System.out.println("------ Fold " + fold + " ------");
+			train(fold);
+			test(fold);
+		}
+
+		return;
+	}
+}
